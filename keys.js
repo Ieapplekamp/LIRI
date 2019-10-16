12d165dbf089c8db62fa310b81e7da522686b1e5
Ian Applekamp <ieapplekamp@gmail.com>
2019-10-16 15:16:03 -0400

    spotify key is working

--- keys.js
+++ keys.js
@@ -1,1 +1,6 @@
 console.log('hi');
+
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+};
--- liri.js
+++ liri.js
@@ -1,1 +1,20 @@
-console.log('hi');
+require("dotenv").config();
+
+var keys = require('./keys.js');
+var Spotify = require('node-spotify-api');
+var axios = require('axios');
+var moment = require('moment');
+
+var spotify = new Spotify(keys.spotify);
+// console.log(spotify);
+var fs = require('fs');
+
+
+function spotifySearch() {
+    spotify.search({
+        track: 'track',
+        query: song
+    }), function (err, data) {
+        
+    }
+}
