4f60dab12225ac72f20edec5107e201c879bfac2
Ian Applekamp <ieapplekamp@gmail.com>
2019-10-17 17:59:42 -0400

    movies is working

--- liri.js
+++ liri.js
@@ -9,7 +9,7 @@ var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 // console.log(spotify);
 var fs = require('fs');
-console.log(keys.spotify);
+// console.log(keys.spotify);
 var command = process.argv[2];
 var search = process.argv.splice(3).join(' ');
 
@@ -19,7 +19,10 @@ function start(command, search) {
             
             spotifySearch(search);
             break;
-            // case: 
+        
+        case "movies-this":
+            omdbMovies(search);
+            break;
             
     }
 
@@ -49,15 +52,49 @@ function spotifySearch(songSearch) {
                     'Artist: ' + results.artists[0].name,
                     'Song: ' + results.name
                 ]
-                console.log('were in' + songSearch);
+                console.log('were in ' + songSearch);
             
-                console.log('please for the love of god' + data);
+                console.log('please for the love of god ' + data);
             }
         }
     )   
 }
-start(command, search);
-// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-//     if (err) {
-//       return console.log('Error occurred: ' + err);
+
+function omdbMovies(search) {
+
+    var queryUrl = "http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy";
+
+    axios.get(queryUrl).then(
+        function(response) {
+          console.log("Release Year: " + response.data.Year);
+        })
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+              } 
+            
+        }
+        )
+}
+
+// function bands(search) {
+
+//     var queryURL = "https://rest.bandsintown.com/artists/" + search + "?app_id=codingbootcamp";
+//     $.ajax({
+//       url: queryURL,
+//       method: "GET"
+//     }).then(function (response) {
+        
 //     }
+
+//     )
+// }
+    
+start(command, search);
