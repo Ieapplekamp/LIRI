59e7fc67141f60c4adb62a9bea79de49455bc99c
Ian Applekamp <ieapplekamp@gmail.com>
2019-10-17 20:39:44 -0400

    trying to get spotify to returm more than one result

--- liri.js
+++ liri.js
@@ -24,40 +24,54 @@ function start(command, search) {
             omdbMovies(search);
             break;
             
+        case "concerts-this":
+            bands(search);
+            break;
     }
 
 }
 
 // console.log(searches);
 
-function spotifySearch(songSearch) {
+function spotifySearch(search) {
 
     
-    if (!songSearch) {
-        songSearch = 'Stressed Out'
+    if (!search) {
+        search = 'Stressed Out'
     }
     
     spotify.search({
         type: 'track',
         query: search,
         limit: 5
-        }, function (err, data) {
-        
-            if (err) {
-                return console.log("Hey there's an error" + err);
-            } else {
-                var results = data.tracks.items[0];
-
-                data = [
-                    'Artist: ' + results.artists[0].name,
-                    'Song: ' + results.name
-                ]
-                console.log('were in ' + songSearch);
+    }, function (err, data) {
             
-                console.log('please for the love of god ' + data);
-            }
+        if (err) {
+            return console.log("Hey there's an error" + err);
+        } else {
+            
+            
+            // for (let i = 0; i < 5; i++) {
+
+            // var results = data.tracks;
+            // console.log(results);
+            // for (let i = 0; i < 5; i++){
+                var results = data.tracks.items[0];
+                data = [
+                    'Song: ' + results.name,
+                    'Album: ' + results.album.name,
+                    // 'Artist: ' + results.items[i].artists[i].name,
+                    
+                    'Artist: ' + results.album.artists[0].name
+                ]
+                    // console.log('were in ' + songSearch);
+                    
+                console.log(data);
+            // }
+
         }
-    )   
+            
+    })   
 }
 
 function omdbMovies(search) {
@@ -84,17 +98,28 @@ function omdbMovies(search) {
         )
 }
 
-// function bands(search) {
+function bands(search) {
 
-//     var queryURL = "https://rest.bandsintown.com/artists/" + search + "?app_id=codingbootcamp";
-//     $.ajax({
-//       url: queryURL,
-//       method: "GET"
-//     }).then(function (response) {
-        
-//     }
+    var queryUrl = "https://rest.bandsintown.com/artists/" + search + "?app_id=codingbootcamp";
+    axios.get(queryUrl).then(
+        function(response) {
+          console.log("Artist: " + response.name);
+        })
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log("---------------Data---------------");
+                console.log(error.response.data);
+                console.log("---------------Status---------------");
+                console.log(error.response.status);
+                console.log("---------------Status---------------");
+                console.log(error.response.headers);
+              } 
+            
+        })
 
-//     )
-// }
+    
+}
     
 start(command, search);
