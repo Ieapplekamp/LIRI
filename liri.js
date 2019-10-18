141dd0d886fc1e919dd5d640db94512da19fc291
Ian Applekamp <ieapplekamp@gmail.com>
2019-10-18 17:54:08 -0400

    fs added and colors

--- liri.js
+++ liri.js
@@ -1,4 +1,3 @@
-
 require("dotenv").config();
 
 var keys = require('./keys.js');
@@ -12,6 +11,13 @@ var fs = require('fs');
 // console.log(keys.spotify);
 var command = process.argv[2];
 var search = process.argv.splice(3).join(' ');
+
+const chalk = require('chalk');
+var white = chalk.hex('#ffffff');
+var blue = chalk.hex('#7FE5F0');
+var pink = chalk.hex('#FF8A9A');
+
+const wrapAnsi = require('wrap-ansi');
 
 function start(command, search) {
     switch (command) {
@@ -60,33 +66,69 @@ function spotifySearch(search) {
                 
                 if (results.preview_url === null) {
 
+                    dataRevisedColor = [
+                        white("\n ------------------| SPOTIFY |--------------------\n") +
+                        blue('* Song: ') + pink(results.name) +  
+                        blue('\n * Album: ') + pink(results.album.name) + 
+                        blue('\n * Artist: ') + pink(results.album.artists[0].name) + 
+                        blue('\n * Song-Preview: ') +  pink('This artist is too much of a diva to share their work :(') + 
+                        white("\n ------------------| SPOTIFY |--------------------\n")
+                    ]
+
                     dataRevised = [
-                        'Song: ' + results.name,
-                        'Album: ' + results.album.name,
-                        'Artist: ' + results.album.artists[0].name,
-                        'Song-Preview: This artist is too much of a diva to share their work'
+                        "\n ------------------| SPOTIFY |--------------------\n" +
+                        '\n * Song: ' + results.name +
+                        '\n * Album: ' + results.album.name + 
+                        '\n * Artist: ' + results.album.artists[0].name + 
+                        '\n * Song-Preview: This artist is too much of a diva to share their work :(' + 
+                        "\n ------------------| SPOTIFY |--------------------\n"
                     ]
 
                     console.log("--------------------------------------");
-                    console.log()
-                    console.log(dataRevised);
+                    console.log(wrapAnsi(dataRevisedColor));
                     console.log("--------------------------------------");
+
+                    fs.appendFile('log.txt', dataRevised, function (err) {
+                        
+                        if (err) {
+                            return console.log(err);
+                        } 
+
+                    });
 
                 } else {
 
+                    dataColor = [
+                        white("\n ------------------| SPOTIFY |--------------------\n") +
+                        blue('\n * Song: ') + pink(results.name) +
+                        blue('\n * Album: ') + pink(results.album.name) +
+                        blue('\n * Artist: ') + pink(results.album.artists[0].name) +
+                        blue('\n * Song-Preview: ') + '\n' + pink(results.preview_url) + '\n' + '\n' +
+                        white("\n ------------------| SPOTIFY |--------------------\n")
+                    ]
+
                     data = [
-                        'Song: ' + results.name,
-                        'Album: ' + results.album.name,
-                        'Artist: ' + results.album.artists[0].name,
-                        'Song-Preview: ' + results.preview_url
+                        '\n * Song: ' + results.name,
+                        '\n * Album: ' + results.album.name,
+                        '\n * Artist: ' + results.album.artists[0].name,
+                        '\n * Song-Preview: ' + results.preview_url
                     ]
             
         
-                    console.log("--------------------------------------");
-                    console.log(data);
-                    console.log("--------------------------------------");
+                    console.log("\r\n");
+                    console.log(wrapAnsi(dataColor));
+                    // logs results to log.txt
+                    
 
+                    fs.appendFile('log.txt', data, function (err) {
+                    
+                        if (err) {
+                        return console.log(err);
+                        } 
+    
+                    });
                 }
+            
             // }
             // console.log(data);
         }
@@ -105,20 +147,35 @@ function omdbMovies(search) {
     axios.get(queryUrl).then(
         function (response) {
 
+            dataColor = [
+                white("\n ------------------| MOVIES |--------------------\n") +
+                blue('\n * Movie Title: ') + pink(response.data.Title) + 
+                blue('\n * Year Released: ') + pink(response.data.Released) + 
+                blue('\n * Run Time: ') + pink(response.data.Runtime) + 
+                blue('\n * IMDB Rating: ') + pink(response.data.imdbRating) + 
+                blue('\n * Rotten Tomatoes Rating: ') + pink(response.data.Ratings[1].Value) + 
+                blue('\n * Country: ') + pink(response.data.Country) + 
+                blue('\n * Language(s): ') + pink(response.data.Language) + 
+                blue('\n * Actors: ') + pink(response.data.Actors) + 
+                blue('\n * Plot: ') + pink(response.data.Plot) + 
+                white("\n ------------------| MOVIES |--------------------\n")
+            ]
+            
             data = [
-                ' * Movie Title: ' + response.data.Title,
-                'Year Released: ' + response.data.Released,
-                'Run Time: ' + response.data.Runtime,
-                'IMDB Rating: ' + response.data.imdbRating,
-                'Rotten Tomatoes Rating: ' + response.data.Ratings[1].Value,
-                'Country: ' + response.data.Country,
-                'Language(s): ' + response.data.Language,
-                'Actors: ' + response.data.Actors,
-                'Plot: '+ response.data.Plot
+                '\n * Movie Title: ' + response.data.Title,
+                '\n * Year Released: ' + response.data.Released,
+                '\n * Run Time: ' + response.data.Runtime,
+                '\n * IMDB Rating: ' + response.data.imdbRating,
+                '\n * Rotten Tomatoes Rating: ' + response.data.Ratings[1].Value,
+                '\n * Country: ' + response.data.Country,
+                '\n * Language(s): ' + response.data.Language,
+                '\n * Actors: ' + response.data.Actors,
+                '\n * Plot: '+ response.data.Plot
+
             ]
 
             console.log("--------------------------------------");
-            console.log(data);
+            console.log(dataColor);
             console.log("--------------------------------------");
 
         })
@@ -135,6 +192,14 @@ function omdbMovies(search) {
               } 
             
         })
+    
+        fs.appendFile('log.txt', data, function (err) {
+            
+            if (err) {
+            return console.log(err);
+            } 
+
+        });
 }
 
 function bands(search) {
@@ -148,20 +213,40 @@ function bands(search) {
     axios.get(queryUrl).then(
         function(response) {
             
+            console.log("\r\n");
             console.log("--------------------------------------");
-            console.log("--------------------------------------");
-            console.log('Next 5 shows for ' + search);
+            console.log("\r\n");
+            console.log(chalk.underline('Next 5 shows for ' + search + ':'));
+            console.log("\r\n");
 
             for (var i = 0; i < 5; i++) {
 
+                dataColor = [
+                    white("\n ------------------| CONCERTS |--------------------\n") +
+                    blue('\n * Venue Location: ') + pink(response.data[i].venue.city) + ', ' + pink(response.data[i].venue.region) +
+                    blue('\n * Venue Name: ') + pink(response.data[i].venue.name) +
+                    blue('\n * Date: ') +pink(moment(response.data[i].datetime).format('MM/DD/YY hh:mm A'))
+                ]
+
                 data = [
-                    'Venue Location: ' + response.data[i].venue.city + ', ' + response.data[i].venue.region,
-                    'Venue Name: ' + response.data[i].venue.name,
-                    'Date: ' + moment(response.data[i].datetime).format('MM/DD/YY hh:mm A')
+                    "\n ------------------| CONCERTS |--------------------\n" +
+                    '\n * Venue Location: ' + response.data[i].venue.city + ', ' + response.data[i].venue.region,
+                    '\n * Venue Name: ' + response.data[i].venue.name,
+                    '\n * Date: ' + moment(response.data[i].datetime).format('MM/DD/YY hh:mm A') + 
+                    "\n ------------------| CONCERTS |--------------------\n"
                 ]
                 
-                console.log(data);
+                console.log(wrapAnsi(dataColor));
+                console.log("\r\n");
                 console.log("--------------------------------------");
+
+                fs.appendFile('log.txt', data, function (err) {
+                    
+                    if (err) {
+                    return console.log(err);
+                    } 
+
+                });
 
             }
             
@@ -206,6 +291,6 @@ start(command, search);
 
 // Things To do 
 
-// 2) use the fs to add logged text pages ** must have
-// 3) try to make it look a bit prettier 
+
+// 3) try to make it look a bit prettier -- mostly check
 // 4) continue working on the spotify api for more results 
